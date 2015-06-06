40ebf1ae468b17b19fbb1839d1db0e1973e57248
Guan Bo <guanbo2002@gmail.com>
2015-06-06 20:46:48 +0800

    Fixed: production datasources

--- server/datasources.production.js
+++ server/datasources.production.js
@@ -2,7 +2,7 @@ module.exports = {
   mysql: {
     hostname: process.env.DB_HOST || 'localhost',
     port: process.env.DB_PORT || 3306,
-    user: process.env.DB_USER,
+    username: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: 'wxcarddb',
     connector: 'mysql'
