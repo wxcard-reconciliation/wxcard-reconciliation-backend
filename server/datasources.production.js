e85c59ab7520534fca0fe146c3ff849e742b9770
Guan Bo <guanbo2002@gmail.com>
2015-06-15 13:14:25 +0800

    Update: production config

--- server/datasources.production.js
+++ server/datasources.production.js
@@ -4,7 +4,7 @@ module.exports = {
     port: process.env.DB_PORT || 3306,
     username: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
-    database: 'wxcarddb',
+    database: process.env.DB_NAME,
     connector: 'mysql'
   }
 };
