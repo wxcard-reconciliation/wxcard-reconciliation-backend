6c92db404231e434cd2c2463543ff58a69073aa2
Guan Bo <guanbo2002@gmail.com>
2016-02-28 20:13:13 +0800

    Update: wechat

--- server/boot/wechat.js
+++ server/boot/wechat.js
@@ -8,8 +8,10 @@ module.exports = function(app) {
    * http://docs.strongloop.com/display/public/LB/Working+with+LoopBack+objects
    * for more info.
    */
-  app.wechat = new WechatAPI(process.env.WX_APPID, process.env.WX_APPSECRET, function (next) {
-    app.models.wxaccesstoken.findOne({where: {appid: process.env.WX_APPID}}, function (err, token) {
+  var appid = process.env.WX_APPID;
+  var appsecret = process.env.WX_APPSECRET;
+  app.wechat = new WechatAPI(appid, appsecret, function (next) {
+    app.models.wxaccesstoken.findOne({where: {appid: appid}}, function (err, token) {
       if(!err && token) {
         next(null, {accessToken: token.accesstoken, expireTime: token.time*1000});
       } else {
@@ -17,17 +19,20 @@ module.exports = function(app) {
       }
     });
   }, function (token, next) {
-    var expireTime = Math.round(token.expireTime/1000)
-    app.models.wxaccesstoken.findOrCreate({where: {appid: process.env.WX_APPID}}, {
-      appid: process.env.WX_APPID,
+    var expireTime = Math.round(token.expireTime/1000);
+    app.models.wxaccesstoken.findOrCreate({where: {appid: appid}}, {
+      appid: appid,
       accesstoken: token.accessToken,
       time: expireTime
     }, function (err, instance) {
       if(err) return next(err);
-      instance.updateAttributes({
-        accesstoken: token.accessToken,
-        time: expireTime
-      }, next)
-    })
+      if(instance) {
+        instance.updateAttributes({
+          accesstoken: token.accessToken,
+          time: expireTime
+        }, next);
+      }
+      else next();
+    });
   });
 };
