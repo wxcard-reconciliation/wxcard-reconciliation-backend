345fb29add65bbb357c5da3a756c5571d7559584
Guan Bo <guanbo2002@gmail.com>
2016-03-23 20:38:54 +0800

    Update: campaign client

--- common/models/campaignclient.js
+++ common/models/campaignclient.js
@@ -3,8 +3,13 @@ module.exports = function(Campaignclient) {
   Campaignclient.observe('before save', function (ctx, next) {
     if(ctx.isNewInstance && ctx.instance) {
       ctx.instance.created = new Date();
+      Campaignclient.app.models.wxclient.fetchUser(ctx.instance.wxclientId, function (err, user) {
+        ctx.instance.wxclient = user || {subscribe: 0, id: ctx.instance.wxclientId};
+        next(err);
+      });
+    } else {
+      next();
     }
-    next();
   });
 
 };
